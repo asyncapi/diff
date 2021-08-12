307ebc054181ee83874b68a0551e3f6d6f660ee3
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-08-12 16:18:47 +0530

    fix tests for diff

--- test/diff.spec.ts
+++ test/diff.spec.ts
@@ -1,9 +1,14 @@
+import { parse } from '@asyncapi/parser';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
 import diff from '../src/diff';
 
 import {
   firstDocument,
   secondDocument,
   diffLocalOutput,
+  diffOutput,
 } from './fixtures/diff.fixtures';
 
 describe('Diff', () => {
@@ -13,5 +18,30 @@ describe('Diff', () => {
 
   test('Check diff output with local inputs', () => {
     expect(diff(firstDocument, secondDocument)).toStrictEqual(diffLocalOutput);
+  });
+
+  test('Check diff output through parser with no difference', async () => {
+    const specDocument = readFileSync(
+      resolve('./test/spec/asyncapi.yml'),
+      'utf-8'
+    );
+    const firstDocument = await parse(specDocument);
+    expect(diff(firstDocument.json(), firstDocument.json())).toStrictEqual([]);
+  });
+
+  test('Check diff output through parser with difference input', async () => {
+    const firstSpecDocument = readFileSync(
+      resolve('./test/spec/asyncapi.yml'),
+      'utf-8'
+    );
+    const secondSpecDocument = readFileSync(
+      resolve('./test/spec/diffSpec.yml'),
+      'utf-8'
+    );
+    const firstDocument = await parse(firstSpecDocument);
+    const secondDocument = await parse(secondSpecDocument);
+    expect(diff(firstDocument.json(), secondDocument.json())).toStrictEqual(
+      diffOutput
+    );
   });
 });
--- test/fixtures/diff.fixtures.ts
+++ test/fixtures/diff.fixtures.ts
@@ -32,3 +32,86 @@ export const diffLocalOutput = [
     after: 'production-dev',
   },
 ];
+
+export const diffOutput = [
+  {
+    action: 'remove',
+    path: '/channels/mychannel',
+    before: {
+      publish: {
+        message: {
+          headers: {
+            properties: {
+              'some-common-header': {
+                type: 'string',
+                'x-parser-schema-id': '<anonymous-schema-2>',
+              },
+            },
+            type: 'object',
+            'x-parser-schema-id': '<anonymous-schema-1>',
+          },
+          schemaFormat: 'application/vnd.aai.asyncapi;version=2.0.0',
+          'x-parser-message-name': 'channelMessage',
+          'x-parser-message-parsed': true,
+          'x-parser-original-traits': [
+            {
+              headers: {
+                properties: {
+                  'some-common-header': {
+                    type: 'string',
+                  },
+                },
+                type: 'object',
+              },
+              'x-some-extension': 'some extension',
+            },
+          ],
+          'x-some-extension': 'some extension',
+        },
+      },
+    },
+  },
+  {
+    action: 'add',
+    path: '/channels/anotherChannel',
+    after: {
+      publish: {
+        message: {
+          headers: {
+            properties: {
+              'some-common-header': {
+                type: 'string',
+                'x-parser-schema-id': '<anonymous-schema-2>',
+              },
+            },
+            type: 'object',
+            'x-parser-schema-id': '<anonymous-schema-1>',
+          },
+          schemaFormat: 'application/vnd.aai.asyncapi;version=2.0.0',
+          'x-parser-message-name': 'channelMessage',
+          'x-parser-message-parsed': true,
+          'x-parser-original-traits': [
+            {
+              headers: {
+                properties: {
+                  'some-common-header': {
+                    type: 'string',
+                  },
+                },
+                type: 'object',
+              },
+              'x-some-extension': 'some extension',
+            },
+          ],
+          'x-some-extension': 'some extension',
+        },
+      },
+    },
+  },
+  {
+    action: 'edit',
+    path: '/info/version',
+    after: '1.1.0',
+    before: '1.0.0',
+  },
+];
