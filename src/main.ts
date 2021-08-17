018e213768d33d6ab8d3dc21105c5ae04be576ea
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-08-17 22:27:01 +0530

    add more jsdoc comments

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,22 @@ import { mergeStandard } from './mergeStandard';
 
 /**
  * Generates diff between two AsyncAPI documents
- * @param firstDocument The parsed AsycnAPI document
- * @param secondDocument The parsed AsycnAPI document
- * @param {Config} config Configuration options
+ * @param firstDocument The parsed AsyncAPI document
+ * @param secondDocument The parsed AsyncAPI document
+ * @param {Object} config Configuration options
+ * @param {Object} [config.override] Object to override the standard
  * @returns {AsyncAPIDiff} The diff data
+ *
+ * @example
+ * const output = diff(firstDocument, secondDocument, {
+ *  override: {
+ *    '/servers': {
+ *      add: 'non-breaking', // when a property has been added in the AsyncAPI document
+ *      remove: 'breaking',  // when a property has been removed from the AsyncAPI document
+ *      edit: 'unclassified' // when a property has been edited in the AsyncAPI document
+ *    }
+ *  }
+ * })
  */
 export function diff(
   firstDocument: any,
